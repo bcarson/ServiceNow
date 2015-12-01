4b304d62cb79b4572076e754daa37a979ad8b26e
Bonnie <bjcarson75@yahoo.com>
2015-12-01 10:32:01 -0600

    Changed first selector

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,28 @@ angular.module('SolutionCenter', [])
     .controller('HomePage', function($scope, JSON_Service, gatherFilters){
         $scope.name = "Cory"; // Placeholder for demo
 
-        var filters = { name:[], industry:[], product:[] };
+        $scope.resetSearch = function(){
+            $scope.currentItem = {name:'solution',industry:'any industry',product:'any product',showAll:false};
+            $scope.pristineForm = true;
+            $scope.showSolutions = false;
+            $scope.showWins = false;
+        }
+
+        $scope.showAll = function(){
+            $scope.currentItem.showAll = true;
+            $scope.showSolutions = true;
+            $scope.showWins = true;
+        }
+        $scope.activateSearch = function(){
+            //$scope.currentItem = {name:'solution',industry:'any industry',product:'any product',showAll:false};
+            $scope.pristineForm = false;
+            $scope.showSolutions = true;
+            $scope.showWins = true;
+        }
+
+        var filters = { name:['solution','solution win'], industry:[], product:[] };
+
+        $scope.resetSearch(); // Initialize search and form values
 
         // Retrieve first data set from JSON file
         JSON_Service.get('solutions.json')
@@ -27,15 +48,15 @@ angular.module('SolutionCenter', [])
                         $scope.filters = filters;
                     });
             });
-        $scope.pristineForm = true; // Hide results until form is activated
-        $scope.currentItem = {name:'solution',industry:'any industry',product:'any product'};
 
         // Natural Language Form control
         $scope.nlFieldOpen= {name:false,industry:false,product:false};
         $scope.nlFieldOpenToggle = function( category, key, value ){
-            if ( key ) {
+            if ( key != undefined ) {
                 $scope.currentItem[category] = value;
                 $scope.pristineForm = false;
+                if(value == 'solution'){ $scope.showSolutions = true; }
+                if(value == 'solution win'){ $scope.showWins = true; }
             }
             $scope.nlFieldOpen[category] = !$scope.nlFieldOpen[category];
         };
@@ -45,9 +66,6 @@ angular.module('SolutionCenter', [])
         // push to array if not duplicate entry
         return {
             checkArrays: function(item, filters){
-                var nameFilter = $.inArray(item.name, filters.name);
-                if(nameFilter == -1){
-                    filters.name.push(item.name); }
                 var industryFilter = $.inArray(item.industry, filters.industry);
                 if(industryFilter == -1){
                     filters.industry.push(item.industry); }
@@ -74,17 +92,29 @@ angular.module('SolutionCenter', [])
     .filter('formSelected', function($filter){
         // Custom Angular Filter to check all three categories simultaneously
         return function (input, selected) {
+            //console.log(input)
+            //console.log(selected)
             var output = [];
-            angular.forEach(input, function(item){
-                if(selected.name == 'solution' || selected.name == item.name){
-                    if(selected.industry == 'any industry' || selected.industry == item.industry){
-                        if(selected.product == 'any product' || selected.product == item.product){
-                            output.push(item);
+            if(selected.showAll){
+                output = input;
+                return output;
+            } else if(selected.search){
+                output = $filter('filter')(input, selected.search);
+                return output;
+            } else {
+                angular.forEach(input, function(item){
+                    //console.log(item);
+                    //if(selected.name == 'solution' || selected.name == item.name){
+                        if(selected.industry == 'any industry' || selected.industry == item.industry){
+                            if(selected.product == 'any product' || selected.product == item.product){
+                                output.push(item);
+                            }
                         }
-                    }
-                }
-            })
-            return output;
+                    //}
+                })
+                return output;
+            }
+
         };
     });
 
